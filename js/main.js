4df57651d5ea21423a122e7572070b09e6cda24d
Daniel Vidar Holm <dannidona3@gmail.com>
2022-11-14 13:20:10 +0100

    Changed the intro text size Changed the padding on the nav items to fill the header Added a eventListener to nav items listening to mouse hover Added a behavour to nav items changing bg and color

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+// Making the navbar reactive to the mouse and changing background when mouse hovers
+const items = document.querySelectorAll('li')
+items.forEach(item => {
+    item.addEventListener('mouseenter', changeBackground)
+    item.addEventListener('mouseleave', changeBackground)
+})
+function changeBackground(e){
+    e.target.classList.toggle('highlight-dark')
+}
